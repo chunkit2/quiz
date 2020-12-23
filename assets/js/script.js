b89a9a2f38488714216b7e25ae5a845d793d2889
Chunkit <chunkit.sharda@yahoo.com>
2020-12-22 18:56:52 -0800

    declared 5 questions as the variable of question

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// declare questions variable.
+var questions = [
+    {
+        q: "String values must be enclosed within _________ when being asigned to variables.", 
+        c: ["quotes", "curly brackets", "parenthesis", "commas"], 
+        a: "quotes"
+    },
+    {
+        q: "The condition in an if/else statement is enclosed within _______.",
+        c: ["quotes", "curly brackets", "square brackets", "parenthesis"],
+        a: "parenthesis"
+    },
+    {
+        q: "A very useful tool for use during development and debugging for printing content to the debugger is _______.",
+        c: ["javascrpt", "terminal/bash", "for loops","console.log"],
+        a: "console.log"
+    },
+    {
+        q: "Commonly used data types do not include _________. ",
+        c: ["strings", "booleans", "alerts", "number"],
+        a: "alerts"
+    },
+    {
+        q: "Arrays in javascript can be used to store ________.",
+        c: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        a: "all of the above"
+    }
+];
